23f6fe37c708035d509298f1ed721f38909ca8a8
Viktoria Bozhko <vbo.secondary@google.com>
2018-12-21 17:43:54 +0300

    commit root and mod7

--- src/main/ClassC.java
+++ src/main/ClassC.java
@@ -1,4 +1,20 @@
 package main;
 
-public class ClassC  {
+import java.util.function.Consumer;
+
+public class ClassC {
+
+    public static void main(String[] args) {
+        System.out.println("Mod 3 ClassC");
+    }
+
+    void test(Consumer<String> foo) {
+
+    }
+
+    public void testHint() {
+        test(tst -> {
+            System.out.println(tst);
+        });
+    }
 }
